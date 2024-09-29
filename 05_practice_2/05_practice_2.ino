a2e1e86a83ad219ecc2d9a13f7a51089d1f1e3f1
paddock0211 <guggenheim7878@gmail.com>
2024-09-30 00:56:43 +0900

    Update 05_practice_2.ino

--- 05_practice_2/05_practice_2.ino
+++ 05_practice_2/05_practice_2.ino
@@ -5,20 +5,20 @@ void setup() {
 }
 
 void loop() {
-    // 1초 동안 LED 켜기
+    
     digitalWrite(LED_PIN, HIGH); 
-    delay(1000); // 1초 대기
+    delay(1000); 
 
-    // LED 끄기 (확인 용도)
+    
     digitalWrite(LED_PIN, LOW); 
     delay(500); 
 
-    // 1초 동안 LED 5회 깜빡이기
+
     for (int i = 0; i < 5; i++) {
         digitalWrite(LED_PIN, HIGH); 
-        delay(100); // 0.1초 대기
+        delay(100); 
         digitalWrite(LED_PIN, LOW); 
-        delay(100); // 0.1초 대기
+        delay(100); 
     }
 
     digitalWrite(LED_PIN, 255);
